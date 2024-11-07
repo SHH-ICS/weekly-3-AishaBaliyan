58de8f2b0a292a2c2b0fb0cc0c12d7aeb5b8043c
Aisha <115123499+AishaBaliyan@users.noreply.github.com>
2024-11-07 12:54:07 +0000

    4+

--- assignment.py
+++ assignment.py
@@ -1,1 +1,22 @@
-
+print("what pizza size do you want? Extra Large or Large")
+pizza = input()
+if pizza == "Large":
+  print("you selected", pizza)
+  Large = 6
+elif pizza == "Extra Large":
+  print("you selected Extra Large")
+  Extra_Large = 10
+print("How many toppings do you want? We only have up to 4 toppings")
+toppings = input()
+if input == 1:
+  price_toppings = 1
+  print("The cost of your pizza is", price_toppings + pizza, "without tax")
+elif input == 2:
+    price_toppings = 1.75
+    print("The cost of your pizza is", price_toppings + pizza, "without tax")
+elif input == 3:
+    price_toppings = 2.50
+    print("The cost of your pizza is", price_toppings + pizza, "without tax")
+elif input == 4:
+    price_toppings = 3.35
+print("The cost of your pizza is", price_toppings + pizza, "without tax")
--- handler.php
+++ handler.php
@@ -1,23 +1,50 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <title>Welcome Page</title>
-  </head>
+<?php
+define("PRICE_LARGE", 6.00);
+define("PRICE_EXTRA_LARGE", 10.00);
+define("PRICE_TOPPING_1", 1.00);
+define("PRICE_TOPPING_2", 1.75);
+define("PRICE_TOPPING_3", 2.50);
+define("PRICE_TOPPING_4", 3.35);
+define("TAX_RATE", 0.13); // 13% HST
 
-  <body>
-    
-    <?php
-    $myVariable = "";
-    if ( isset( $_POST['myVariable'] ) ){
-      $myVariable = $_POST['myVariable'];
-    }
-    echo "<h1>My Program</h1>\n";
-    echo "<p>My Variable is = ".$myVariable."</p>\n";
-    ?>
-    
-  </body>
-  
-</html>
+$size = strtolower(trim($_POST['size']));
+$toppings = intval($_POST['toppings']);
+
+if ($size !== "large" && $size !== "extra large") {
+  die("Plesase choose either 'Large' or 'Extra Large' for the pizza size");
+}
+
+if ($toppings < 1 || $toppings > 4) {
+  die("Please choose a number of toppings between 1 and 4, no more and no less");
+}
+
+$base_price = ($size == "large") ? PRICE_LARGE : PRICE_EXTRA_LARGE;
+
+switch ($toppings) {
+  case 1:
+    $topping_price = PRICE_TOPPING_1;
+    break;
+  case 2:
+    $topping_price = PRICE_TOPPING_2;
+    break;
+  case 3:
+    $topping_price = PRICE_TOPPING_3;
+    break;
+  case 4:
+    $topping_price = PRICE_TOPPING_4;
+    break;
+  default:
+    $topping_price = 0;
+}
+
+$subtotal = $base_price + $topping_price;
+
+$tax = $subtotal * TAX_RATE;
+$total_cost = $subtotal + $tax;
+
+echo "<h2>Order Summary</h2>";
+echo "Pizza Size: " . ucfirst($size) . "<br>";
+echo "Number of Toppings: $toppings<br>";
+echo "Subtotal: $" . number_format($subtotal, 2) . "<br>";
+echo "Tax (13% HST): $" . number_format($tax, 2) . "<br>";
+echo "Total Cost: $" . number_format($total_cost, 2);
